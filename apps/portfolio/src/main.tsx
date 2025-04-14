2d3f3e34a319d713d01a61d8bf24a2a68eb78b67
Hareesh Devulapalli <hareeshdevulapalli777@gmail.com>
2025-04-13 21:17:56 -0500

    added url related in deploy file

--- apps/portfolio/src/main.tsx
+++ apps/portfolio/src/main.tsx
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/portfolio">
       <App />
     </BrowserRouter>
   </StrictMode>
